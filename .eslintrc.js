5ec948ddf3554d72871b4e2bf457dad7691f0b5d
feizhijun <feizhijun@bytedance.com>
2019-02-17 16:30:34 +0800

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "globals": {
+        "document": true,
+        "foo": true,
+        "window": true
+    },
+    "rules":{
+        "indent": [2, "tab"],
+        "no-tabs": 0
+    }
+};
