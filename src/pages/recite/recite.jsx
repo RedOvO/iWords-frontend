9f73ce0297346fb2b71715dad85d0299f5197d81
RedOvO <599236422@qq.com>
2019-02-19 18:29:36 +0800

    add part of wordpane's style

--- src/pages/recite/recite.jsx
+++ src/pages/recite/recite.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
 	Layout,
-	Tabs
+	Tabs,
+	Progress
 } from 'antd';
 import 'antd/dist/antd.css';
 import './recite.css';
@@ -16,13 +17,41 @@ class Recite extends Component {
 				<div className="recite_container">
 					<Tabs defaultActiveKey="1" type="card">
 						<TabPane tab="四级" key="1">
-							<div className="cet4_container">
-								四级
+							<div className="pane_container">
+								<div className="word_container">
+									<div className="one_word_container">
+										Hello CET4!
+									</div>
+								</div>
+								<div className="progress_container">
+									<div className="progress">
+										<span>今日背诵进度</span>
+										<Progress type="circle" percent={77} />
+									</div>
+									<div className="progress">
+										四级背诵进度
+										<Progress type="circle" percent={42} />
+									</div>
+								</div>
 							</div>
 						</TabPane>
 						<TabPane tab="六级" key="2">
-							<div className="cet6_container">
-								六级
+							<div className="pane_container">
+								<div className="word_container">
+									<div className="one_word_container">
+										Hello CET6!
+									</div>
+								</div>
+								<div className="progress_container">
+									<div className="progress">
+										<span>今日背诵进度</span>
+										<Progress type="circle" percent={66} />
+									</div>
+									<div className="progress">
+										六级背诵进度
+										<Progress type="circle" percent={53} />
+									</div>
+								</div>
 							</div>
 						</TabPane>
 					</Tabs>
